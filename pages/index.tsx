b24236fef1915dcaf4e7e5bf3a47ca632e82d1d6
jitunayak <jitunayak715@gmail.com>
2023-04-08 19:09:00 +0530

    vercel deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,9 +35,6 @@ const Home: NextPage = () => {
             rel="noopener noreferrer"
           >
             Thank you for visiting my profile
-            {/* <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span> */}
           </a>
         </footer>
       </div>
